7a65b94eed6d727e83521468ac36abb41180682a
Jose Daniel Perez <jose_daniel05@hotmail.com>
2019-06-21 19:54:13 -0400

    Commit Cambio 1

---
+++ Practica-2/Services/StudentServices.cs
@@ -0,0 +1,84 @@
+﻿using Persistence;
+
+namespace Services
+{
+    public interface IStudentService {
+        IEnumerable<Student> GetAll(Student model);
+        IEnumerable<Student> GetAll(Student model);
+        IEnumerable<Student> GetAll(Student model);
+        IEnumerable<Student> GetAll(Student model);
+
+
+    }
+    public class StudentServices: IStudentService
+    {
+        private readonly StudentDbContext _studenteDbContext;
+        public StudentServices(StudentDbContext _studentDbContext)
+        {
+            _studentDbContext = studentDbContext; 
+        }
+
+        public IEnumerable<Student> GetAll(Student model)
+        {
+            var result = new List<Student>();
+            try
+            {
+                result = _studenteDbContext.Student.ToList(); 
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+            return result;
+        }
+        public bool Add(StudentServices model)
+        {
+            try
+            {
+                _studenteDbContext.Add(model);
+                _studenteDbContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public bool Update(StudentServices model)
+        {
+            try
+            {
+                var originalModel = _studenteDbContext.Student.Single(x => x.StudentID == model.StudentId);
+
+                originalModel.Name = model.Name;
+                originalModel.LastName = model.LastName;
+                originalModel.Bio = model.bio;
+                _studenteDbContext.Update(model);
+                _studenteDbContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                _studenteDbContext.Entry(new Student { StudentId=id}).Stage=EntityStage.Deleted; 
+                _studenteDbContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
