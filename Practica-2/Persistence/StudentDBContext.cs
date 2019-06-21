0e818f8feff61804d4d0a61a8cf2e542deb06a05
Jose Daniel Perez <jose_daniel05@hotmail.com>
2019-06-21 16:40:33 -0400

    Primer commit del projecto

---
+++ Practica-2/Persistence/StudentDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class StudentDBContext : DbContext 
+    {
+        public DbSet<Student> Student { get; set; }
+
+        public StudentDBContext(DbContextOptions<StudentDBContext> options):base(options)
+        { }
+    }
+}
