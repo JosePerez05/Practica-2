746f640137169ebb133586ee380f43d0d5b94818
Jose Daniel Perez <jose_daniel05@hotmail.com>
2019-06-21 21:05:47 -0400

    Commit Cambio 2

--- Practica-2/Services/StudentServices.cs
+++ Practica-2/Services/StudentServices.cs
@@ -4,9 +4,10 @@ namespace Services
 {
     public interface IStudentService {
         IEnumerable<Student> GetAll(Student model);
-        IEnumerable<Student> GetAll(Student model);
-        IEnumerable<Student> GetAll(Student model);
-        IEnumerable<Student> GetAll(Student model);
+        bool Add(StudentServices model);
+        boll Delete(int id);
+        boll Update(StudentServices model);
+        Student Get(int id);
 
 
     }
@@ -18,12 +19,27 @@ namespace Services
             _studentDbContext = studentDbContext; 
         }
 
+        public Student Get(int id)
+        {
+            var result = new Student();
+            try
+            {
+                result = _studenteDbContext.Student.Single(x => x.StudentId == id); 
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+            return result;
+        }
+
         public IEnumerable<Student> GetAll(Student model)
         {
             var result = new List<Student>();
             try
             {
-                result = _studenteDbContext.Student.ToList(); 
+                result = _studenteDbContext.Student.ToList();
             }
             catch (System.Exception)
             {
@@ -65,7 +81,6 @@ namespace Services
 
             return true;
         }
-
         public bool Delete(int id)
         {
             try
